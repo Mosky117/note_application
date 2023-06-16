bb572d59447e4a787fd7ce43fa7b2504b9993170
Mosky117 <davi.mosky@outlook.com>
2023-06-16 22:53:09 +0200

    first commit

---
+++ notes_project.dpr
@@ -0,0 +1,14 @@
+program notes_project;
+
+uses
+  Vcl.Forms,
+  notes in 'notes.pas' {Note};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TNote, Note);
+  Application.Run;
+end.
